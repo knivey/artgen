10281122943389a37d0eeb931fa10871bdc38fe8
Fred Akers <knivey@botops.net>
2022-12-22 21:52:50 -0500

    add [][] access on canvas and put better example in main

--- canvas.hpp
+++ canvas.hpp
@@ -5,19 +5,34 @@
 #include <string>
 #include "pixel.hpp"
 
+class canvasCol {
+    std::vector<std::vector<pixel>> &data;
+    unsigned int col;
+public:
+    canvasCol(std::vector<std::vector<pixel>> &data, unsigned int col) : data(data), col(col) {};
+    pixel& operator[] (unsigned int row) {
+        return data[row][col];
+    };
+};
+
 class canvas
 {
-public:
-    /**
-     * Default constructor
-     */
-    canvas(unsigned int width, unsigned int height);
-
+private:
     std::vector<std::vector<pixel>> data;
     unsigned int width;
     unsigned int height;
-
+public:
+    canvas(unsigned int width, unsigned int height);
     std::string render(bool IRC = false);
+    canvasCol operator[] (unsigned int col) {
+        return canvasCol(data, col);
+    };
+    unsigned int getWidth() {
+        return width;
+    }
+    unsigned int getHeight() {
+        return height;
+    }
 };
 
 #endif // CANVAS_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <math.h>
 
 #include "canvas.hpp"
 
 
 int main(int argc, char **argv) {
     canvas canvas(80,20);
-
-    canvas.data[10][5].c = "x";
+    char s[] = "lol";
+    for(unsigned int x = 0; x < 80; ++x) {
+        unsigned int y = 10 + round((sin(x/6.0)*5.0));
+        canvas[x][y].c = s[x%(sizeof(s)-1)];
+        canvas[x][y].fg = (x%12) +52;
+    }
 
     std::cout << canvas.render(false);
     return 0;
